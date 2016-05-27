73a7dd7bae5308db85aea5d1664bd038d2366c64
Max Pernkau <max.pernklau@udo.edu>
2016-05-27 15:38:57 +0200

    Script for getting client code from github to localhost

---
+++ getLoginScript.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+source_server="https://github.com/DimensionalScoop/"
+repo="pool-client"
+script="main.bash"
+
+git clone "$source_server$repo" #try to clone
+if [ "$?" -ne "0" ]
+	then #local copy of repo probably already exists
+	cd $repo
+	git reset --hard
+	git pull
+	cd ..
+fi
+
+cd $repo
+bash $script
