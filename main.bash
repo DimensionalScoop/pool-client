6bd0972a71ce6579e1511abce1a71d93c7613b37
Max Perklau <max.pernklau@udo.edu>
2016-10-27 18:32:49 +0200

    x2goclient finished?

--- main.bash
+++ main.bash
@@ -4,5 +4,7 @@
 
 server="stud.physik.tu-dortmund.de"
 
-x2goclient --thinclient 
-# --no-menu --maximize --session="Stud" --no-session-edit --add-to-known-hosts
+mkdir ~/.x2goclient
+cp sessions ~/.x2goclient/sessions
+
+x2goclient --thinclient  --no-menu --maximize --session="Stud" --no-session-edit --add-to-known-hosts
