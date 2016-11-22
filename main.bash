42ad1f70b3d96481503cc29a13d4cc09b35aa154
Max Perklau <max.pernklau@udo.edu>
2016-11-22 20:39:57 +0100

    Restart whole x2goclient when user logs out.

--- main.bash
+++ main.bash
@@ -7,4 +7,4 @@ server="stud.physik.tu-dortmund.de"
 mkdir ~/.x2goclient
 cp sessions ~/.x2goclient/sessions
 
-x2goclient --thinclient  --no-menu --maximize --session="Stud" --no-session-edit --add-to-known-hosts
+x2goclient --thinclient  --no-menu --maximize --session="Stud" --no-session-edit --add-to-known-hosts --close-disconnect
